088c91f9cd58b467b02169e98ed9d7c2e9dd6627
Shelikhov <shelikhov.evgenii.21@gmail.com>
2023-03-03 10:42:44 +0000

    removed network interface from launch template

--- aws/modules/ec2-module/main.tf
+++ aws/modules/ec2-module/main.tf
@@ -67,9 +67,9 @@ resource "aws_launch_template" "ec2_linux_template" {
   instance_type = var.instance_type
   key_name      = aws_key_pair.ec2_key_pair.key_name
   user_data     = var.file_user_data #filebase64("${var.file_user_data}")
-  network_interfaces {
-    network_interface_id = aws_network_interface.net_interface.id
-  }
+#  network_interfaces {
+#    network_interface_id = aws_network_interface.net_interface.id
+#  }
   tags = var.tags
 }
 
